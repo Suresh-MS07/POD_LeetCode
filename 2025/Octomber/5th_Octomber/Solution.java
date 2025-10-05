4cb4783c99f60906ef1300ea751c56517a4bd04d
SURESH MEWADA <sureshmewada990@gmail.com>
2025-10-05 14:58:37 +0530

    Day 4

---
+++ 2025/Octomber/5th_Octomber/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length, n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0, heights[i][0]);
+            dfs(heights, atlantic, i, n - 1, heights[i][n - 1]);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j, heights[0][j]);
+            dfs(heights, atlantic, m - 1, j, heights[m - 1][j]);
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j])
+                    res.add(Arrays.asList(i, j));
+            }
+        }
+        return res;
+    }
+
+    private void dfs(int[][] h, boolean[][] ocean, int i, int j, int prev) {
+        if (i < 0 || j < 0 || i >= h.length || j >= h[0].length) return;
+        if (ocean[i][j] || h[i][j] < prev) return;
+        ocean[i][j] = true;
+        dfs(h, ocean, i + 1, j, h[i][j]);
+        dfs(h, ocean, i - 1, j, h[i][j]);
+        dfs(h, ocean, i, j + 1, h[i][j]);
+        dfs(h, ocean, i, j - 1, h[i][j]);
+    }
+}
